09b8221ccd3f1afe9feb47fe9dc236a3fcdc6899
David Rivero <guest-developer@Juans-MacBook-Pro.local>
2016-04-29 12:43:02 -0300

    Emergencies

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -53,24 +53,24 @@ angular.module('starter.controllers', [])
 })
 
 .controller('MapCtrl', function($scope) {
-    var citymap = {
-      chicago: {
-        center: {lat: 41.878, lng: -87.629},
-        population: 2714856
-      },
-      newyork: {
-        center: {lat: 40.714, lng: -74.005},
-        population: 8405837
-      },
-      losangeles: {
-        center: {lat: 34.052, lng: -118.243},
-        population: 3857799
-      },
-      vancouver: {
-        center: {lat: 49.25, lng: -123.1},
-        population: 603502
-      }
-    };
+    // var citymap = {
+    //   chicago: {
+    //     center: {lat: 41.878, lng: -87.629},
+    //     population: 2714856
+    //   },
+    //   newyork: {
+    //     center: {lat: 40.714, lng: -74.005},
+    //     population: 8405837
+    //   },
+    //   losangeles: {
+    //     center: {lat: 34.052, lng: -118.243},
+    //     population: 3857799
+    //   },
+    //   vancouver: {
+    //     center: {lat: 49.25, lng: -123.1},
+    //     population: 603502
+    //   }
+    // };
 
     var map = new google.maps.Map(document.getElementById('map'), {
         center: { lat: -34.397, lng: 150.644 },
@@ -80,17 +80,17 @@ angular.module('starter.controllers', [])
 
     // Construct the circle for each value in citymap.
   // Note: We scale the area of the circle based on the population.
-  for (var city in citymap) {
-      // Add the circle for this city to the map.
-      var cityCircle = new google.maps.Circle({
-        strokeColor: '#FF0000',
-        strokeOpacity: 0.8,
-        strokeWeight: 2,
-        fillColor: '#FF0000',
-        fillOpacity: 0.35,
-        map: map,
-        center: citymap[city].center,
-        radius: Math.sqrt(citymap[city].population) * 100
-      });
-  }
+  // for (var city in citymap) {
+  //     // Add the circle for this city to the map.
+  //     var cityCircle = new google.maps.Circle({
+  //       strokeColor: '#FF0000',
+  //       strokeOpacity: 0.8,
+  //       strokeWeight: 2,
+  //       fillColor: '#FF0000',
+  //       fillOpacity: 0.35,
+  //       map: map,
+  //       center: citymap[city].center,
+  //       radius: Math.sqrt(citymap[city].population) * 100
+  //     });
+  // }
 });
